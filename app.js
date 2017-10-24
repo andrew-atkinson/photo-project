b87e33797e3be3ab8cb5912bd1ecb8d44b844bc1
andrew-atkinson <blind.sam@gmail.com>
2017-10-24 17:57:01 -0400

    adds: more tutorial stuff

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var apos = require('apostrophe')({
-  shortName: 'photo-project',
-  title: 'photo-project',
+  shortName: 'photo-project', title: 'photo-project',
 
   // See lib/modules for basic project-level configuration of our modules
   // responsible for serving static assets, managing page templates and
@@ -8,5 +7,17 @@ var apos = require('apostrophe')({
 
   modules: {
     // Add custom apostrophe-modules and their respective configuration here!
-  }
+    'apostrophe-pages': {
+      types: [
+        {
+          name: 'default',
+          label: 'Default'
+        }, {
+          name: 'home',
+          label: 'Home'
+        }
+      ]
+    }
+  },
+  // This configures our default page template
 });
